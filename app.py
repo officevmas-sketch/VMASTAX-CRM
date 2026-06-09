b7b989da21936e7a522c69e29fd5b6ed84df0aa0
officevmas-sketch <office.vmas@gmail.com>
2026-06-09 16:26:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -649,6 +649,7 @@ st.markdown(f"""
 <div class='powerbi-header'>
   <div>
    <div>
+   <div>
     <div class='powerbi-title'>VMAS CRM Analytics Dashboard</div>
   </div>
   <div class='powerbi-badge'>Amount View: {amount_unit}</div>
